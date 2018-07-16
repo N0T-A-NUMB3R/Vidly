fa029d24b03b6acee6e14e765243b0243898ea9f
Fab Nan <notanumber@hotmail.it>
2018-07-16 16:02:02 +0200

    Aggiunto il controller Customers

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,48 +1,46 @@
 ﻿using System.Collections.Generic;
 using System.Web.Mvc;
-using Microsoft.Ajax.Utilities;
 using Vidly.Models;
 using Vidly.ViewModel;
+
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
+
         // GET: Movies/Random
         public ActionResult Random()
         {
-            var movie = new Movie() {Name = "Shrek"};
-            var customer = new List<Customer>
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
             {
-                new Customer() {Name = "Customer1"},
-                new Customer() {Name = "Customer2"}
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
             };
+
             var viewModel = new RandomMovieViewModel
             {
                 Movie = movie,
-                Customers = customer
+                Customers = customers
             };
+
             return View(viewModel);
-
-        }
-        //[Route("movies/released/{year}/{month:regex(\\d{4}:range(1,12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            return Content($"{year} --- {month}");
-        }
-
-
-        public ActionResult Edit(int id)
-        {
-            return Content($"Id ={id}");
-        }
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (string.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-            return Content($"page index ={pageIndex} and sortby ={sortBy}");
         }
     }
 }
